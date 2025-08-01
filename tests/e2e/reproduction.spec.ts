4f7098790212e5947cf6ee1d92c231a1cf8993d9
Carsten Lindner <131704054+2K9FTwsc6Dt6@users.noreply.github.com>
2025-08-01 21:46:55 +0200

    Explain the unexpected behavior

--- tests/e2e/reproduction.spec.ts
+++ tests/e2e/reproduction.spec.ts
@@ -12,9 +12,25 @@ test('unexpected ionic sheet modal screenshot behavior', async ({ page }) => {
 
   await page.locator('ion-modal.fully-visible').waitFor();
 
-  // The Ionic Sheet Modal is visible on this screenshot...
+  // The Ionic Sheet Modal is visible on this Playwright screenshot...
   await page.screenshot({ path: 'screenshot.png' });
 
   // ... but NOT on this one!
   await expect(page).toHaveScreenshot();
+
+  // This is because of different default values for "animations":
+  // PageAssertions "toHaveScreenshot(options)"
+  // has a default value of "disabled" for animations, whereas
+  // the regular full page screenshot API "screenshot(options)"
+  // has a default value of "allow".
+
+  // The Ionic Sheet Modal obviously creates some kind of infinite animation
+  // that is canceled to the initial state,
+  // as explained in the Playwright documentation,
+  // and this way the modal is effectively closed,
+
+  // So adding the option "{ animations: 'allow' }"
+  // makes everything work as expected!
+
+  // "await expect(page).toHaveScreenshot({ animations: 'allow' });"
 });
